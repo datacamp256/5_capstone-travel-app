1b4d9100b9a6b4b60b05d3efc97f5cd386db773c
Data Camp256 <68553874+datacamp256@users.noreply.github.com>
2020-09-28 14:50:15 +0200

    feat: get temperature with default ZIP from OWM
    #16

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
 let owmApiKey;
+const BUTTON_ID = 'generate';
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
@@ -20,8 +21,22 @@ document.addEventListener('DOMContentLoaded', async function () {
 });
 
 // Event listener to add function to existing HTML DOM element
+document.getElementById(BUTTON_ID).addEventListener('click', generateEntry);
+
+async function loadTemperature(zipCode) {
+    console.log(`Get the temperature for US ZIP ${zipCode} from openweathermap.org.`)
+    let result = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=85051,us&appid=${owmApiKey}`)
+        .then(response => response.json())
+        .then(data => data.main.temp);
+    console.log(result);
+    return result;
+}
 
 /* Function called by event listener */
+function generateEntry() {
+    const temperature = loadTemperature();
+    console.log(temperature);
+}
 
 /* Function to GET Web API Data*/
 
