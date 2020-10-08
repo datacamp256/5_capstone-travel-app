730486c3b9ba95886ff73a0a17191c07b21dedad
Data Camp256 <68553874+datacamp256@users.noreply.github.com>
2020-10-08 21:10:47 +0200

    refactor: rename server.js

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/client/index.js',
+    entry: './src/client/server.js',
     module: {
         rules: [
             {
