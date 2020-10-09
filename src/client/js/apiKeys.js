23d99ee71854543fde3530e1942f55a249551e9c
Data Camp256 <68553874+datacamp256@users.noreply.github.com>
2020-10-09 10:11:00 +0200

    chore: cleanup
    #5

--- src/client/js/apiKeys.js
+++ src/client/js/apiKeys.js
@@ -4,7 +4,6 @@ async function loadGeonamesApiKey() {
    await fetch('http://localhost:8081/loadApiKey?application=geonames')
         .then(response => response.text())
         .then(text => geonamesApiKey = text);
-    console.log(geonamesApiKey);
 }
 const getGeonamesApiKey = () => geonamesApiKey;
 
